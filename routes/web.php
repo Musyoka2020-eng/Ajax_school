ec2510a2b10b0adf8b2cb28830fc2ec430c96189
Musyoka2020-eng <musyokanzau3@gmail.com>
2022-08-08 03:27:19 +0300

    First commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+Route::group(['middleware' => 'auth'], function() {
+    Route::group(['middleware' => 'role:student', 'prefix' => 'student', 'as' => 'student.'], function() {
+        Route::resource('lessons', \App\Http\Controllers\Student\LessonController::class);
+    });
+   Route::group(['middleware' => 'role:teacher', 'prefix' => 'teacher', 'as' => 'teacher.'], function() {
+       Route::resource('courses', \App\Http\Controllers\Teachers\CourseController::class);
+   });
+    Route::group(['middleware' => 'role:admin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
+        Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+    });
+});
